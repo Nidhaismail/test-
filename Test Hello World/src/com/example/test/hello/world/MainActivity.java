35ae3c09719d5d5455b75ac435b399107bf75c66
Nidhaismail <nidha.farooq.ismail@gmail.com>
2012-10-07 21:24:13 -0700

    addingHellowWorld

---
+++ Test Hello World/src/com/example/test/hello/world/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.test.hello.world;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        View button1 = findViewById(R.id.button1);
+        button1.setBackgroundColor(0xffff0000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    
+    
+}
